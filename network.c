ee22e58dbe55d28baa8d93635b8db8f0e30d12f1
Alvin Lin <jiaweilin0923@gmail.com>
2020-12-17 18:27:28 +0800

    Update

--- network.c
+++ network.c
@@ -33,41 +33,6 @@
 #define IPV6_ADDR_MAPPED 0x1000U
 #define IPV6_ADDR_RESERVED 0x2000U /* reserved address space */
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <netinet/if_ether.h>
-#include <netinet/in.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <ifaddrs.h>
-#include <linux/ethtool.h>
-#include <linux/sockios.h>
-#include <errno.h>
-
-#define _PATH_PROCNET_DEV "/proc/net/dev"
-#define _PATH_PROCNET_IFINET6 "/proc/net/if_inet6"
-#define _PATH_PROCNET_ROUTE "/proc/net/route"
-
-#define MAX_IF_COUNT 10
-#define MAX_HW_ADDR_STR_LENGTH 18
-
-#define IPV6_ADDR_ANY 0x0000U
-#define IPV6_ADDR_UNICAST 0x0001U
-#define IPV6_ADDR_MULTICAST 0x0002U
-#define IPV6_ADDR_ANYCAST 0x0004U
-#define IPV6_ADDR_LOOPBACK 0x0010U
-#define IPV6_ADDR_LINKLOCAL 0x0020U
-#define IPV6_ADDR_SITELOCAL 0x0040U
-#define IPV6_ADDR_COMPATv4 0x0080U
-#define IPV6_ADDR_SCOPE_MASK 0x00f0U
-#define IPV6_ADDR_MAPPED 0x1000U
-#define IPV6_ADDR_RESERVED 0x2000U /* reserved address space */
-
 /**
 *@fn get_if_hw_addr
 *@brief Get MAC address with particular interface name
